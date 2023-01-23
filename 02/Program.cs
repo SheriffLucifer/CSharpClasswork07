e1bb15aa29286d0c71598791fdc5a6a1ef349f21
Oleg <olegashepelev@gmail.com>
2023-01-23 23:37:00 +0600

    Fix first task

--- 02/Program.cs
+++ 02/Program.cs
@@ -1,29 +1,21 @@
-﻿// Задайте двумерный массив размера m на n, 
-// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
-// Выведите полученный массив на экран.
-// m = 3, n = 4.
-// 0 1 2 3
-// 1 2 3 4
-// 2 3 4 5
-
-Console.Clear();
+﻿Console.Clear();
 Console.Write("Введите количество строк массива: ");
 
 int rows = int.Parse(Console.ReadLine() ?? "");
 Console.Write("Введите количество столбцов массива: ");
 
 int columns = int.Parse(Console.ReadLine() ?? "");
-int[,] array = GetArray(rows, columns);
+int[,] array = GetArray(rows, columns, 0, 10);
 PrintArray(array);
 
-int[,] GetArray(int m, int n)
+int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
     int[,] result = new int[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = i + j;
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
         }
     }
     return result;
