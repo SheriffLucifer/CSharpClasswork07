d25bb92e8eb6633ae97f266084da0f69f7bb5bd9
Oleg <olegashepelev@gmail.com>
2023-01-24 01:11:00 +0600

    Fix second and third tasks

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,10 +1,11 @@
-﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
-// и замените эти элементы на их квадраты.
-// Например, изначально массив
-//  выглядел вот так:
+﻿// Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
 
 Console.Clear();
 Console.Write("Введите количество строк массива: ");
@@ -15,10 +16,9 @@ Console.Write("Введите количество столбцов массив
 int columns = int.Parse(Console.ReadLine() ?? "");
 int[,] array = GetArray(rows, columns, 0, 10);
 PrintArray(array);
-Console.WriteLine("--------------------");
 
-int[,] newArray = CheckArray(array);
-PrintArray(newArray);
+int sum = GetSumDiagonal(array);
+Console.WriteLine(sum);
 
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
@@ -33,24 +33,24 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
     return result;
 }
 
-int[,] CheckArray(int[,] inArray)
+int GetSumDiagonal(int[,] inArray)
 {
-    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
-    for (int i = 0; i < array.GetLength(0); i++)
+    int sum = 0;
+    // int resolutionMin = Math.Min(array.GetLength(0), array.GetLength(1));
+    int size = 0;
+    if (inArray.GetLength(0) < inArray.GetLength(1))
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if (i % 2 == 0 && j % 2 == 0)
-            {
-                result[i, j] = array[i, j] * array[i, j];
-            }
-            else
-            {
-                result[i, j] = array[i, j];
-            }
-        }
+        size = inArray.GetLength(0);
     }
-    return result;
+    else
+    {
+        size = inArray.GetLength(1);
+    }
+    for (int i = 0; i < size; i++)
+    {
+        sum += inArray[i, i];
+    }
+    return sum;
 }
 
 void PrintArray(int[,] inArray)
